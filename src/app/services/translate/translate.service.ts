0ba163e15d80a0217e18dfe7aea0815a7d972632
GrigoriyMo <38403456+GrigoriyMo@users.noreply.github.com>
2022-12-05 12:57:01 +0300

    Update translate.service.ts

--- src/app/services/translate/translate.service.ts
+++ src/app/services/translate/translate.service.ts
@@ -20,7 +20,7 @@ export class TranslateService {
     let httpHeaders = new HttpHeaders();
     httpHeaders = httpHeaders.set('content-type', 'application/x-www-form-urlencoded');
     httpHeaders = httpHeaders.append('Accept-Encoding', 'application/gzip');
-    httpHeaders = httpHeaders.append('X-RapidAPI-Key', 'e263c85038msh59b3b1b54125060p161c1djsnaee8f2d34ecb');
+    httpHeaders = httpHeaders.append('X-RapidAPI-Key', '');
     httpHeaders = httpHeaders.append('X-RapidAPI-Host', 'google-translate1.p.rapidapi.com');
 
     return this.http.post<string>(url, encodedParams, { headers: httpHeaders });
